07a4ffafac0dab9b0a9ba58a107f83157c204032
Karoline <150620236+karol003@users.noreply.github.com>
2024-02-28 15:25:30 -0300

    Add files via upload

--- pages/Supermercado.js
+++ pages/Supermercado.js
@@ -25,7 +25,7 @@ const SuperMercado = ({ Component, pageProps }) => {
 
     <>
     <Head>
-      <title>SuperMercado</title>
+      <title>Supermercado</title>
     </Head>
     <div className={styles.bori}>
 
